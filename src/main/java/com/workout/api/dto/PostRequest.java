878a7d4e72163fd0057b807b496ce8b5c5308d10
jayhug2 <jayhug2@daum.net>
2026-01-06 22:39:45 +0900

    Post 엔티티 및 CRUD API 구현 (#11)

---
+++ src/main/java/com/workout/api/dto/PostRequest.java
@@ -0,0 +1,18 @@
+package com.workout.api.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostRequest {
+
+    @NotBlank(message = "제목은 필수입니다")
+    @Size(max = 200, message = "제목은 200자 이하여야 합니다")
+    private String title;
+
+    @NotBlank(message = "내용은 필수입니다")
+    private String content;
+}
