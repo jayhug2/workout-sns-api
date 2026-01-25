5aa9b643b0e75db7e4e2d2ad38772c32ebac3a05
jayhug2 <jayhug2@daum.net>
2026-01-25 16:17:52 +0900

    Async 비동기 설정 추가 (#35)

---
+++ src/main/java/com/workout/api/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.workout.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
